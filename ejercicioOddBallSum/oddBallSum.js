4693f5df253bfd6f23401b937692b1e7b5ee9dfd
Alvaro Frias <alvarofriasruiz03@gmail.com>
2023-12-18 13:59:29 +0100

    ejercicio de sumar todos los impares

--- ejercicioOddBallSum/oddBallSum.js
+++ ejercicioOddBallSum/oddBallSum.js
@@ -5,7 +5,14 @@
  */
 
 function oddball_sum(nums) {
-  //Escribe tu codigo aqui
+	let total = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] % 2 !== 0) {
+			total += nums[i];
+		}
+	}
+
+	return total;
 }
 
 module.exports = { oddball_sum };
