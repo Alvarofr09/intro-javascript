8e692c603ea11ccf5343fdf745890acff50802f2
Alvaro Frias <alvarofriasruiz03@gmail.com>
2023-12-18 14:00:08 +0100

    ejercicio de saber si 3 elementos consecutivos suman 7

--- ejercicioSum7/sum7.js
+++ ejercicioSum7/sum7.js
@@ -4,8 +4,18 @@
  * y retorne true si 3 elementos consecutivos suman 7
  */
 
-function lucky_sevens() {
-  //Escribe tu codigo aqui
+function lucky_sevens(array) {
+	if (array.length > 2) {
+		for (let i = 0; i <= array.length - 3; i++) {
+			const sumaConsecutiva = array[i] + array[i + 1] + array[i + 2];
+
+			if (sumaConsecutiva === 7) {
+				return true;
+			}
+		}
+	}
+
+	return false;
 }
 
 module.exports = { lucky_sevens };
