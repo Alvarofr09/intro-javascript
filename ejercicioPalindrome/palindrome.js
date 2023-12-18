b5e99e4bed5c10b5c29ce62f8858356ee110a34b
Alvaro Frias <alvarofriasruiz03@gmail.com>
2023-12-18 13:59:45 +0100

    ejercicios de saber si es palindromo o no

--- ejercicioPalindrome/palindrome.js
+++ ejercicioPalindrome/palindrome.js
@@ -1,14 +1,16 @@
-/* 1. Comprobar si un string es palindromo.
-Se dice que un string es palindromo cuando se puede leer igual de izquierda a derecha que de derecha a izquierda. */
-
 /**
  * Ejercicio Palindrome
  * Generar un algoritmo que nos compruebe si el string introducido es un palindromo. Se dice que un
  * string es palindromo cuando se puede leer igual de izquierda a derecha que de derecha a izquierda
  */
 
-function palindrome() {
-  //Escribe tu codigo aqui
+function palindrome(frase) {
+	var re = /[\W_]/g;
+	let frase1 = frase.toLowerCase().replace(re, "");
+
+	let fraseReversed = frase1.split("").reverse().join("");
+
+	return frase1 === fraseReversed;
 }
 
 module.exports = { palindrome };
